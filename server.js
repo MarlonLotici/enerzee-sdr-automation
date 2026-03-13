e6ff8fe75cc99ffd1524289e084c1597c968f68d
MarlonLotici <marlonlotici6@gmail.com>
2026-03-13 02:16:58 -0300

    Corrigindo sintaxe de rota curinga no Express

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ let shouldStop = false;
 // Isso faz o backend entregar a tela visual do seu painel
 // ============================================================================
 app.use(express.static(path.join(__dirname, 'dist')));
-app.get('*', (req, res) => {
+app.get('/(.*)', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 // ============================================================================
